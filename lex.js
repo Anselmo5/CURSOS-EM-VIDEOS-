ebda61f07290c56f76edd805c5159f0ae83c7648
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2022-08-12 22:53:36 -0300

    Delete lex.js

--- lex.js
+++
@@ -1,4 +0,0 @@
-//window.alert('está gostando do curso?')
-//window.confirm('está gostando do curso de js')
-var nome = window.prompt('qual é seu nome?')
-window.alert('Prazer em conhecelo ^^' + nome) // + serve como contatenação(junção)  de duas coisas no js
