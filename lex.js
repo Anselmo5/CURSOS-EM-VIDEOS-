8990403287aa6092bf86ec12733f7945432e2438
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2022-08-12 22:52:56 -0300

    Add files via upload

---
+++ lex.js
@@ -0,0 +1,4 @@
+//window.alert('está gostando do curso?')
+//window.confirm('está gostando do curso de js')
+var nome = window.prompt('qual é seu nome?')
+window.alert('Prazer em conhecelo ^^' + nome) // + serve como contatenação(junção)  de duas coisas no js
